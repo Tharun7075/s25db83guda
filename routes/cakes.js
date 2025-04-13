d55e9b8fe21f8fc19393fdaae8ece49ee460bac1
Tharun Guda <tharunguda7@gmail.com>
2025-04-13 00:31:44 -0500

    done

--- routes/cakes.js
+++ routes/cakes.js
@@ -14,6 +14,19 @@ router.get('/', cakes_controllers.cakes_view_all_Page );
 // GET request for one costume.
 router.get('/cakes/:id', cakes_controllers.cakes_detail);
 router.get('/cakes/:id', cakes_controllers.cakes_update_put);
+
+// Handle Costume delete on DELETE.
+exports.cakes_delete = async function(req, res) {
+  console.log("delete " + req.params.id)
+  try {
+  result = await cakes.findByIdAndDelete( req.params.id)
+  console.log("Removed " + result)
+  res.send(result)
+  } catch (err) {
+  res.status(500)
+  res.send(`{"error": Error deleting ${err}}`);
+  }
+ };
 module.exports = router;
 
 module.exports = router;
