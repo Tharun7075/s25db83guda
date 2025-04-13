3363e0dc91ed6b992e4a4a64e198d255d213963f
Tharun Guda <tharunguda7@gmail.com>
2025-04-13 10:26:32 -0500

    done

--- controllers/cakes.js
+++ controllers/cakes.js
@@ -97,6 +97,19 @@ failed`);
  }
 };
 
+// Handle Costume delete on DELETE.
+exports.cakes_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await cakes.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
 
 
 // Handle Costume update form on PUT.
--- routes/cakes.js
+++ routes/cakes.js
@@ -15,18 +15,7 @@ router.get('/', cakes_controllers.cakes_view_all_Page );
 router.get('/cakes/:id', cakes_controllers.cakes_detail);
 router.get('/cakes/:id', cakes_controllers.cakes_update_put);
 
-// Handle Costume delete on DELETE.
-exports.cakes_delete = async function(req, res) {
-  console.log("delete " + req.params.id)
-  try {
-  result = await cakes.findByIdAndDelete( req.params.id)
-  console.log("Removed " + result)
-  res.send(result)
-  } catch (err) {
-  res.status(500)
-  res.send(`{"error": Error deleting ${err}}`);
-  }
- };
+
 module.exports = router;
 
 module.exports = router;
