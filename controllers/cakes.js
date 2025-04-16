4fc5a9e5d0d0bbba49fd0c0ac7addf128b260776
Tharun Guda <tharunguda7@gmail.com>
2025-04-15 19:35:37 -0500

    Changes

--- controllers/cakes.js
+++ controllers/cakes.js
@@ -109,6 +109,19 @@ exports.cakes_delete = async function(req, res) {
     res.send(`{"error": Error deleting ${err}}`);
     }
    };
+   // Handle a show one view with id specified by query
+exports.cakes_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await cakes.findById( req.query.id)
+    res.render('cakesdetail',
+   { title: 'cakes Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
 
 
 
@@ -133,3 +146,45 @@ else toUpdate.same = false;
 failed`);
  }
 };
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.cakes_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('cakescreate', { title: 'cakes Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   //Handle building the view for updating a costume.
+   // query provides the id
+   exports.cakes_update_Page = async function(req, res) {
+   console.log("update view for item "+req.query.id)
+   try{
+   let result = await cakes.findById(req.query.id)
+   res.render('cakesupdate', { title: 'cakes Update', toShow: result });
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+   };
+
+   // Handle a delete one view with id from query
+exports.cakes_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await cakes.findById(req.query.id)
+    res.render('cakesdelete', { title: 'cakes Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/cakes.js
+++ routes/cakes.js
@@ -14,6 +14,22 @@ router.get('/', cakes_controllers.cakes_view_all_Page );
 // GET request for one costume.
 router.get('/cakes/:id', cakes_controllers.cakes_detail);
 router.get('/cakes/:id', cakes_controllers.cakes_update_put);
+/* GET detail costume page */
+router.get('/detail', cakes_controllers.cakes_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', cakes_controllers.cakes_create_Page);
+
+/* GET create update page */
+router.get('/update', cakes_controllers.cakes_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', cakes_controllers.cakes_delete_Page);
+
+
+
+
+
 
 
 module.exports = router;
